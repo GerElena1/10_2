29d2e3f389dc9c2c598df2159360f7f129fdf6ee
Elena <Polsh_el@mail.ru>
2024-05-21 12:56:39 +0300

    10_4

--- src/pages/ToDo.vue
+++ src/pages/ToDo.vue
@@ -1,6 +1,12 @@
 <template>
     <q-page class="bg-grey-6 column">
-        <h5>ToDo</h5>
+        <div class="row q-pa-sm bg-blue-3">
+            <q-input @keyup.enter="addTask" filled class="col shadow-8" square bg-color="blue-3" v-model="newTask"
+                placeholder="Add task" dense>
+                <template v-slot:append>
+                    <q-btn @click="addTask" round dense flat icon="add"></q-btn></template>
+            </q-input>
+        </div>
         <q-list separator bordered>
             <q-item @click="task.done = !task.done" clickable :class="{ 'done bg-green-5': task.done }"
                 v-for="task, index in tasks" :key="task.title" v-ripple>
@@ -22,11 +28,30 @@
 export default {
     data() {
         return {
+            newTask: "",
             tasks: [{ title: '', done: false }, { title: 'helo1', done: false }, { title: 'helo2', done: false }]
         }
     },
     methods: {
-        deleteTask(index) { this.tasks.splice(index, 1) }
+        deleteTask(index) {
+            this.$q.dialog({
+                title: 'Confirm',
+                message: 'Delete?',
+                cancel: true,
+                persistent: true
+            }).onOk(() => {
+                this.tasks.splice(index, 1);
+                this.$q.notify('Task delete.')
+            })
+
+        },
+        addTask() {
+            this.tasks.push({
+                title: this.newTask,
+                done: false
+            });
+            this.newTask = "";
+        }
     }
 }
 </script>
